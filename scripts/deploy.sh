4c62cb8e7409cd3dbda92a14cd8179af5c95450e
zhuziyi <zhuziyi1989@gmail.com>
2021-12-02 08:56:16 +0800

    🥕 部署地址修正

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,13 +11,13 @@ rm -rf $TEMP_PATH
 mkdir $TEMP_PATH
 cd $TEMP_PATH
 git init
-git pull https://github.com/zhuziyi1989/front-guidebook.git gh-pages
+git pull git@github.com:zhuziyi1989/front-guidebook.git gh-pages
 cp -r ../../dist/* .
 
 # commit and push changes
 git add -A
 git commit --am -m "build: deploy documentation"
-git push -f https://github.com/zhuziyi1989/front-guidebook.git master:gh-pages
+git push -f git@github.com:zhuziyi1989/front-guidebook.git master:gh-pages
 
 ## clean
 cd
