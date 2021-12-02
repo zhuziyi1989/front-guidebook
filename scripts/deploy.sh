4f6396bf22652c1820b124e3de3d86ab8ececcaa
zhuziyi <zhuziyi1989@gmail.com>
2021-12-02 19:00:56 +0800

    修复部署脚本格式不同系统的兼容问题

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,18 @@
+set -e
+shopt -s extglob
+
+TEMP_PATH="docs/.temp"
+
 # build docs
 npm run build
 
-cd dist
+# prepare deploy
+rm -rf $TEMP_PATH
+mkdir $TEMP_PATH
+cd $TEMP_PATH
+git init
+git pull git@github.com:zhuziyi1989/front-guidebook.git gh-pages
+cp -r ../../dist/* .
 
 # commit and push changes
 git add -A
@@ -9,5 +20,5 @@ git commit --am -m "build: deploy documentation"
 git push -f git@github.com:zhuziyi1989/front-guidebook.git master:gh-pages
 
 ## clean
-cd ..
-rm -rf dist
+cd
+rm -rf $TEMP_PATH
